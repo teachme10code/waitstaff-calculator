153252545f931350fde605d715f6381f2758e252
teachme10code <chris.ward@servicenow.com>
2015-06-11 22:35:13 -0700

    completed project

--- app.js
+++ app.js
@@ -2,12 +2,37 @@
 
 angular.module('myApp', [])
 	.controller('StaffCalculator', function($scope) {
+		var meal_count = 0;
+		var average_tip = 0;
+		var tip = 0;
 
-		$scope.calculate = function() {
-			$scope.data.subtotal = $scope.data.meal_price * $scope.data.tax_rate * $scope.data.tip_percentage;
+		$scope.calculateCharges = function() {
+			$scope.data.tax = $scope.data.meal_price * ($scope.data.tax_rate * .01);
+			$scope.data.subtotal = $scope.data.meal_price + $scope.data.tax;
+			$scope.data.tip = $scope.data.subtotal * ($scope.data.tip_percentage * .01);
+			tip += $scope.data.tip;
+			$scope.data.total = $scope.data.subtotal + $scope.data.tip;
+			meal_count += 1;
+			$scope.calculateEarnings();
+		};
+
+		$scope.calculateEarnings = function() {
+			$scope.data.tips_total = tip;
+			$scope.data.meal_count = meal_count;
+			$scope.data.average_tip = $scope.data.tips_total / meal_count;
+			$scope.clearInputFields();
+		};
+
+		$scope.clearInputFields = function() {
+			MealForm.reset();
 		};
 
 		$scope.resetForm = function() {
+			meal_count = 0;
+			average_tip = 0;
+			tip = 0;
 			$scope.data = {};
+			MealForm.reset();
+			$scope.MealForm.$setPristine();
 		};
 	})
